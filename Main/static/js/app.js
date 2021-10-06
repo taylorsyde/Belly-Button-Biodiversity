b09f6045df885a38a61490180b53a42bd9b7d563
taylorsyde <tay.lyons@hotmail.com>
2021-10-05 23:32:07 -0500

    gauge now responsive

--- Main/static/js/app.js
+++ Main/static/js/app.js
@@ -16,34 +16,22 @@ function init(){
         popBar(initId);
         popDemo(initId);
         popBubble(initId);
-
-
-        // var idData = data.samples.filter(foo => foo.id == currentId)[0];
-        // d3.select('#bar').html(''); // clears the current plot
-        // let barData = [{
-        //     type: 'bar',
-        //     x: idData.sample_values.slice(0,10),
-        //     // y: idData.otu_ids.slice(0,10),
-        //     y: idData.otu_labels.slice(0,10),
-        //     orientation: 'h'}];
-        // let barLayout = {
-        //     title: "Cultures in Belly Button",
-        //     height: 300,
-        //     width: 400};
-        // Plotly.newPlot('bar', barData, barLayout);
+        popGauage(initId)
     })
 };
 
+// initialize the visulaizations
 init();
 
+// formatting fuction
 function capitalize(string) {
 return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// populate Demographics Panel
 function popDemo(selected){
     d3.json('samples.json').then(data => {
         let demoData = data.metadata;
-        console.log(demoData);
         var currentDemo = demoData.filter(foo => foo.id == selected)[0];
         console.log(currentDemo);
         var demoPanel = d3.select(".panel-body")
@@ -54,7 +42,7 @@ function popDemo(selected){
     });
 };
 
-
+// populates Bar graph
 function popBar(selected){
     d3.json('samples.json').then(data => {
         var currentId = data.samples.filter(foo => foo.id == selected)[0];
@@ -73,6 +61,7 @@ function popBar(selected){
     });
 };
 
+// populate Bubble Chart
 function popBubble(selected){
     d3.json('samples.json').then(data => {
         var currentId = data.samples.filter(foo => foo.id == selected)[0];
@@ -81,20 +70,22 @@ function popBubble(selected){
         let updateBubble = [{
             x: currentId.otu_ids,
             y: currentId.sample_values,
-            // text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
             mode: 'markers',
             marker: {
-                color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+                color: 'YlOrRd',
                 size: currentId.sample_values}
         }];
         let layout = {
-            title: 'Bubble Chart Hover Text',
+            title: `Cultures Present in Subject ${currentId.id}'s Bellybutton`,
             height: 500,
-            width: 1000};
+            width: 1000,
+            xaxis: {title: "Operational Taxonomic Unit (OTU)"}
+        };
         Plotly.newPlot('bubble', updateBubble, layout);
     });
 };
 
+// updates visualizations when id is changed
 function optionChanged(){
     console.log('option function triggered')
     var currentId = d3.select("#selDataset").property("value");
@@ -102,54 +93,6 @@ function optionChanged(){
     popBar(currentId);
     popDemo(currentId);
     popBubble(currentId);
+    popGauage(currentId);
 };
-
-
-// // Call optionChaged when a change takes place to the DOM
-// d3.selectAll("#selDataset").on("change", optionChanged);
-
-
-// function popBubble(selectedId){};
-
-// // Initializes the page with a default plot
-// function updateDemos() {
-//     d3.json("samples.json").then(dataGrab2 => {
-//         var 
-    
-
-//     var dataID = mData.filter(data => data.id == demographics);
-//     console.log(dataID)        
-// }
-
-// updateDemos();
-
-
-// // Call updatePlotly() when a change takes place to the DOM
-// d3.selectAll("#selDataset").on("change", optionChanged);
-
-// // This function is called when a dropdown menu item is selected
-// function updatePlotly() {
-//   // Use D3 to select the dropdown menu
-//   var dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a variable
-//   var dataset = dropdownMenu.property("value");
-
-//   // Initialize x and y arrays
-//   var x = [];
-//   var y = [];
-
-//   if (dataset === 'dataset1') {
-//     x = [1, 2, 3, 4, 5];
-//     y = [1, 2, 4, 8, 16];
-//   }
-
-//   else if (dataset === 'dataset2') {
-//     x = [10, 20, 30, 40, 50];
-//     y = [1, 10, 100, 1000, 10000];
-//   }
-
-//   // Note the extra brackets around 'x' and 'y'
-//   Plotly.restyle("plot", "x", [x]);
-//   Plotly.restyle("plot", "y", [y]);
-// }
-
+  
--- Main/static/js/gauge.js
+++ Main/static/js/gauge.js
@@ -1,7 +1,44 @@
-var data = [
-    {
+// // ///// BONUS ATTEMPT HERE
+// function popGauage(selected){
+//   d3.json('samples.json').then(data => {
+//     let demoData = data.metadata;
+//     let currentDemo = demoData.filter(foo => foo.id == selected)[0];
+//     let washCount = currentDemo.wfreq;
+// //     // formatting for gauage
+// //     let data = [{
+// //       domain: { x: [0, 1], y: [0, 1] },
+// //       value: washCount,
+// //       title: { text: "Belly Button Washing Frequency" },
+// //       type: "indicator",
+// //       mode: "gauge",
+// //       gauge: {
+// //         axis: { range: [null, 10] },
+// //         steps: [
+// //           { range: [0, 1], color: "lightgray" },
+// //           { range: [1, 2], color: "gray" },
+// //           { range: [2, 3], color: "lightgray" },
+// //           { range: [3, 4], color: "gray" },
+// //           { range: [5, 6], color: "lightgray" },
+// //           { range: [6, 7], color: "gray" },
+// //           { range: [7, 8], color: "lightgray" },
+// //           { range: [8, 9], color: "gray" },
+// //           { range: [9, 10], color: "lightgray" }] // close steps
+// //         }//close gauge
+// //     }];//close data
+// //     let layout = { width: 300, height: 200, margin: { t: 0, b: 0 }};
+// //     Plotly.newPlot('gauge', data, layout);
+// //   });
+// // };
+
+
+function popGauage(selected){
+  d3.json('samples.json').then(data => {
+  let demoData = data.metadata;
+  let currentDemo = demoData.filter(foo => foo.id == selected)[0];
+  let washCount = currentDemo.wfreq;
+  var data = [{
       domain: { x: [0, 1], y: [0, 1] },
-      value: 6,
+      value: washCount,
       title: { text: "Belly Button Washing Frequency" },
       type: "indicator",
       mode: "gauge",
@@ -16,18 +53,11 @@ var data = [
           { range: [6, 7], color: "gray" },
           { range: [7, 8], color: "lightgray" },
           { range: [8, 9], color: "gray" },
-          { range: [9, 10], color: "lightgray" }
-        ]
-        
-        // threshold: {
-        // line: { color: "red", width: 4 },
-        // thickness: 0.75,
-        // value: 490
-        // }
-      }
+          { range: [9, 10], color: "lightgray" }]}
     }
   ];
-  
   var layout = { width: 300, height: 200, margin: { t: 0, b: 0 } };
   Plotly.newPlot('gauge', data, layout);
-  
+
+  });
+};
